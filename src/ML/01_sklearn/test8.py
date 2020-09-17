8894efd8aa773a33487ab6ff90c4b0f4144ec076
whitestarrain <55945266+whitestarrain@users.noreply.github.com>
2020-09-17 22:01:10 +0800

    移动笔记

--- src/ML/01_sklearn/test8.py
+++ src/ML/01_sklearn/test8.py
@@ -1,6 +1,7 @@
 from sklearn.decomposition import PCA
 import numpy as np
 import pandas as pd
+import requests
 
 def test():
     pass
@@ -9,6 +10,7 @@ def test():
 def main():
     pca = PCA(n_components=0.9)
     rdata = pca.fit_transform([[2, 8, 4, 5], [6, 3, 0, 8], [5, 4, 9, 1]])
+    np.random
     print(rdata)
 
 if __name__ == "__main__":
